842474efd4b53d7a11ee5de7ee2871e5b7bfa638
tqsmmm <tqsmmm@gmail.com>
2023-02-15 10:46:17 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import axios from 'axios';
+import { onMounted } from 'vue';
+
+onMounted(() => {
+    axios
+        .get('https://devapi.qweather.com/v7/weather/now', {
+            params: {
+                key: 'c42a982f075a47c192e0db18cc805a6a',
+                location: '101010100',
+            },
+        })
+        .then(res => {
+            console.log(res.data);
+        });
+});
+</script>
+
+<template></template>
+
+<style></style>
